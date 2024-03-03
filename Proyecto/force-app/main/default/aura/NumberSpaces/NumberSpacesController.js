0d7ec5f2fdbcaf7fe6cf84d6012950534885d783
LiamRamiro <ramiro.emlia22@zaragoza.salesianos.edu>
2024-03-03 12:58:19 +0100

    error en NumberSpaces

--- Proyecto/force-app/main/default/aura/NumberSpaces/NumberSpacesController.js
+++ Proyecto/force-app/main/default/aura/NumberSpaces/NumberSpacesController.js
@@ -1,5 +1,21 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    doInit : function(component, event, helper) {
+        var randomNumber = Math.floor(Math.random() * 100) + 1; // Genera un número aleatorio entre 1 y 100
+        component.set("v.randomNumber", randomNumber);
+    },
+
+    handleNumberButtonClick : function(component, event, helper) {
+        var number = event.getParam("number");
+        var numbers = component.get("v.numbers");
+        numbers.push(number);
+        component.set("v.numbers", numbers);
+
+        var sum = numbers.reduce(function(acc, curr) {
+            return acc + parseInt(curr);
+        }, 0);
+        component.set("v.sum", sum);
     }
 })
